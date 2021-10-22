1ac3ad57d0c7f5e6e341e96218639f33230405c5
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-22 10:00:06 -0400

    Add file

---
+++ quiz_1.py
@@ -0,0 +1,29 @@
+#Ryan Lugo: RJL 10/22/21
+
+q = int(input("What day is it?: "))
+m = int(input("What Month number is it?: "))
+j = int(input("What is the century?: "))
+k = j % 100
+
+day = "None"
+
+day_formula = (q + ((26 *(m + 1)) / 10) + k + (k / 4) + (j / 4) + (5 * j)) % 7
+
+if day_formula == 0:
+    day = "Saturday"
+elif day_formula ==1:
+    day = "Sunday"
+elif day_formula == 2:
+    day = "Monday"
+elif day_formula == 3:
+    day = "Tuesday"
+elif day_formula == 4:
+    day = "Wednesday"
+elif day_formula == 5:
+    day = "Thursday"
+elif day_formula == 6:
+    day = "Friday"
+else:
+    print("Error",day_formula)
+
+print(q,"/",m,"/",j,"was a",day)
